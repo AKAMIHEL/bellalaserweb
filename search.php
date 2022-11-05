9cab56aa626d15473d96baa3bb34f69204f590d0
AKAMIHEL <98933111+AKAMIHEL@users.noreply.github.com>
2022-11-04 22:50:41 -0500

    Add files via upload

---
+++ search.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search Results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/touchTouch.css">
+    <link rel="stylesheet" href="search/search.css">
+    <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-migrate-1.1.1.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+
+        <script src="search/search.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/touchTouch.jquery.js"></script>
+        <script src="js/jquery.ui.totop.js"></script>
+    <!--[if lt IE 8]>
+        <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+    <![endif]-->
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="css/ie.css">
+    <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!-- header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="span12">                
+<!--menu-->
+                <div class="navbar">
+                     <div class="navbar-inner"> 
+                        <div class="nav-collapse nav-collapse_ collapse">
+                            <ul class="sf-menu">
+                                <li class="sub-menu"><a href="index.html"><span>About us</span></a><strong></strong>
+                                     <ul class="nav sub-menu">
+                                        <li><a href="#">mission</a></li>
+                                        <li><a href="#">our recipes</a></li>
+                                        <li class="last sub-menu"><a href="#">news</a>
+                                            <ul class="nav sub-menu">
+                                                <li><a href="#">fresh</a></li>
+                                                <li class="last"><a href="#">archive</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="index-1.html"><span>Nutrition </span></a><strong></strong></li>
+                                <li><a href="index-2.html"><span>Diets</span></a><strong></strong></li>
+                                <li><a href="index-3.html"><span>Programs</span></a><strong></strong></li>
+                                <li class="last"><a href="index-4.html"><span>Contacts</span></a><strong></strong></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt="new shape the best weight loss plan"></a></h1>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- content -->
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <hr class="margin1">
+                <h2>Search Results:</h2>
+                <div id="search-results"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- footer -->
+<footer>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="span4">
+                     <h2>fresh recipes</h2>
+                     <ul class="list3">
+                         <li><a href="img/footer_pic1_big.jpg" class="thumbnail"><img src="img/footer_pic1.jpg" alt=""></a></li>
+                         <li><a href="img/footer_pic2_big.jpg" class="thumbnail"><img src="img/footer_pic2.jpg" alt=""></a></li>
+                         <li class="right"><a href="img/footer_pic3_big.jpg" class="thumbnail"><img src="img/footer_pic3.jpg" alt=""></a></li>
+                         <li><a href="img/footer_pic4_big.jpg" class="thumbnail"><img src="img/footer_pic4.jpg" alt=""></a></li>
+                         <li><a href="img/footer_pic5_big.jpg" class="thumbnail"><img src="img/footer_pic5.jpg" alt=""></a></li>
+                         <li class="right"><a href="img/footer_pic6_big.jpg" class="thumbnail"><img src="img/footer_pic6.jpg" alt=""></a></li>
+                     </ul>
+                     <p><a href="#" class="btn btn-link">view more<span></span></a></p>
+                 </div> 
+                 <div class="span3 offset1">
+                       <h2>results of the week</h2>
+                       <ul class="list4"><li><a href="#">01. maria lee</a></li>
+                       <li><a href="#">02. samantha smith</a></li>
+                       <li><a href="#">03. anna beldi</a></li>
+                       <li><a href="#">04. dan brown</a></li>
+                       <li><a href="#">05. martha rogers</a></li>
+                       <li><a href="#">06. tommy hamilton</a></li></ul>
+                 </div>  
+                 <div class="span3 offset1">
+                        <h2>locations</h2>
+                        <address>28 Jackson Blvd Ste 1020 <br>Chicago  <br>IL 60604-2340</address>
+                        <hr>
+                        <address class="mail"><a href="#"><!--email_off-->info@demolink.org<!--/email_off--></a></address>
+                        <ul class="soc_icons">
+                            <li><a href="#"><img src="img/soc_icon1.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/soc_icon2.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/soc_icon3.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/soc_icon4.png" alt=""></a></li>
+                        </ul>
+                 </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="span6 floatRight1200">
+                    <ul class="links"><li><a href="index.html">About us</a></li>
+                    <li><a href="index-1.html">Nutrition</a></li>
+                    <li><a href="index-2.html">Diets</a></li>
+                    <li><a href="index-3.html">Programs</a></li>
+                    <li><a href="index-4.html">Contacts</a></li></ul>
+                </div>
+                <div class="span6 floatLeft1200">
+                    <p class="privacy">new shape &copy; 2013 <a href="index-5.html">Privacy policy</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.js"></script>
+<script>
+  jQuery(document).ready(function($) {
+    $('.list3>li>a')
+            .prepend('<span class="sitem_over"><strong></strong></span>')
+            .touchTouch();
+  });
+</script>
+</body>
+</html>
